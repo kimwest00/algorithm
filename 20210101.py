650ac58d1a69fcd8ac0912c7f9acca168f585174
김민서 <kim-west00@naver.com>
2021-01-01 23:50:24 +0900

    재귀함수방법 오류 수정

--- 20210101.py
+++ 20210101.py
@@ -24,17 +24,18 @@ def gcd_mod(a, b):
 	else:
 		return b
 # 재귀 함수를 이용
-# 재귀 함수 타임에러
+# 재귀 함수 타임에러 -> 재귀함수 까먹어서 32번째 줄에 return을 안써주고 호출만...
+# (처음에 호출된 함수의 return 값과 함수안에서 호출된 함수의 return 값이 달라짐)
 def gcd_rec(a, b):
-	while(a*b != 0):
+	if (a*b != 0):#while할 필요없이 바로 함수호출혹은 return으로 넘어가므로 if!
 		if(a>b):
-			gcd_rec(a%b,b)
+			return gcd_rec(a%b,b)
 		else:
-			gcd_rec(a,b%a)
+			return gcd_rec(a,b%a)
 	if(a*100 != 0):
-		return b
-	else:
 		return a
+	else:
+		return b
 	
 # a, b를 입력받는다
 # gcd_sub, gcd_mod, gcd_rec을 각각 호출하여, x, y, z에 리턴값을 저장한다
