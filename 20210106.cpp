94a5e0516cd3bad6c7c0a0586739cbe4e66c0dda
김민서 <kim-west00@naver.com>
2021-01-06 23:49:30 +0900

    문자열세기

---
+++ 20210106.cpp
@@ -0,0 +1,11 @@
+//BoJ 2743 https://www.acmicpc.net/problem/2743
+#include <iostream>
+using namespace std;
+#include <string>
+int main(){
+    string N;
+    cin >> N;
+    cout << sizeof(N);
+
+    return 0;
+}
