4d3fc4e2a53393f2c672589b1e657d8223fbed2a
김민서 <kim-west00@naver.com>
2021-01-11 23:47:05 +0900

    오류수정완료

--- 20210111.cpp
+++ 20210111.cpp
@@ -3,11 +3,7 @@
 #include <iostream>
 using namespace std;
 /*
-오류 1) test[]배열에서 대실된 시간대를 -1로 바꾸는 과정
-(ex 0 1 이면 0만 -1로 바꿔야함<= 1부터는 대실이 가능하기때문)
-오류 2)이중 for문 고려 x
-break로 밖에있는 for문 탈출은 안된다고 생각했지만,
-cout 되고 끝난다고 생각
+오류3)
 */
 int main(){
     int test_num,test_time,total_time;
@@ -24,7 +20,7 @@ int main(){
     int count = 0;
     int finish=0;
     int save;
-    while(finish == -1)//오류2)함수처럼 출력(return)후 종료하기위해 while문 기능 추가
+    while(finish != -1){//오류3)
         for(int i=0;i<total_time;i++){
             if(test[i]!=-1){
                 save =i;
@@ -37,16 +33,20 @@ int main(){
                     finish = -1;
             }
         }
-        for(int j=save;j<=save+total_time;j++){//i를 불러오지 못하므로 save변수 활용
+        }
+        for(int j=save;j<save+total_time;j++){//i를 불러오지 못하므로 save변수 활용
             if(test[j]==-1){
                 cout<<-1<<endl;
-                finish = -1;
+                break;
                 }
-            if(j==total_time+save)
+            if(j==save+test_time-1)
                 cout<<save<<endl;
+        
         }
+        break;
     
 }
+    
 
     return 0;
 
